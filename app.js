ba6dde80c46884d9063cfc1703d68c34ca1d314e
joaogritzenco <gritzenco.merlo@escola.pr.gov.br>
2025-10-22 08:50:49 -0300

    .

---
+++ app.js
@@ -0,0 +1,5 @@
+function criaCartao(categoria, pergunta, resposta){
+    let comntainer= document.getElementByld('container')
+    let cartao= document.createElement('article')
+    cartao.className = 'cartao'
+}
